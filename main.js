8d4617fa8f60d2cd3b2959d1cba91214960dc61e
mahmoud-mohamed22 <mahmoud.azaro2984@gmail.com>
2026-06-15 04:01:31 +0300

    destinations
    add map

--- main.js
+++ main.js
@@ -4,23 +4,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (bookingForm && submitBtn) {
         bookingForm.addEventListener("submit", (e) => {
-            e.preventDefault();
+            e.preventDefault(); 
 
+    
             const explorerName = document.getElementById("explorer-name").value;
-            const selectedDestination = document.getElementById("destination").options[document.getElementById("destination").selectedIndex].text;
             const explorerAge = document.getElementById("explorer-age").value;
             const explorerCountry = document.getElementById("explorer-country").value;
             const explorerPhone = document.getElementById("explorer-phone").value;
 
             localStorage.setItem("explorerName", explorerName);
-            localStorage.setItem("chosenDestination", selectedDestination);
             localStorage.setItem("explorerAge", explorerAge);
             localStorage.setItem("explorerCountry", explorerCountry);
             localStorage.setItem("explorerPhone", explorerPhone);
             submitBtn.style.pointerEvents = "none";
-            submitBtn.innerHTML = `OPENING TIME VORTEX... <span class="spinner">⏳</span>`;
+            submitBtn.innerHTML = `OPENING MAP GATEWAY... <span class="spinner">⏳</span>`;
             submitBtn.style.background = "linear-gradient(135deg, #bfa169 0%, #8c6f39 100%)";
             submitBtn.style.color = "#0d0906";
+
             setTimeout(() => {
                 window.location.href = "destinations.html";
             }, 2000);
